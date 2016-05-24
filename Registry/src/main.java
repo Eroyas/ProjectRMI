3ad9fb2f36b975153f842c956c24965a796d31e1
Corentin <corentin.fouyaya@hotmail.fr>
2016-05-24 15:38:50 +0200

    [update] main de RMI

--- Registry/src/main.java
+++ Registry/src/main.java
@@ -1,9 +1,10 @@
-import iRMI.IRMIRegistry;
 import RMI.RMIRegistry;
+import iRMI.IRMIRegistry;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.security.AccessControlException;
 
 /**
  * Created by Eroyas on 22/05/16.
@@ -11,11 +12,24 @@ import java.rmi.RemoteException;
 public class main {
     public static void main(String[] args) {
         try {
+            if (System.getSecurityManager() != null)
+                System.out.println("Security Manager already Activated !");
+            else {
+                System.out.println("We activate Security Manager !");
+                System.setSecurityManager(new SecurityManager());
+            }
+            // we verify that security manager works
+            System.getSecurityManager().checkAccept("localhost", 8080);
+
             IRMIRegistry rmi = new RMIRegistry();
-            Naming.rebind("rmi://localhost:4000/MyRMI", rmi);
+
+            Naming.rebind("rmi://localhost:8080/MyRMI", rmi);
             System.out.println("RMI configured !");
         } catch (RemoteException | MalformedURLException e) {
             e.printStackTrace();
+        }catch(AccessControlException e ){
+            System.out.println("Sorry, we can't acces to localhost:1234 (rmiregistry). \nHave you configured your Security file ?");
         }
+
     }
 }
