8f10dc9b669736beef20376aa20668b79ae966cd
Corentin <corentin.fouyaya@hotmail.fr>
2016-05-25 20:08:41 +0200

    TestJMS

--- Services/src/LibraryService/ServiceLibrary.java
+++ Services/src/LibraryService/ServiceLibrary.java
@@ -7,7 +7,7 @@ import java.rmi.Naming;
 /**
  * Created by Eroyas on 22/05/16.
  */
-public class ServiceLibrary {
+public class ServiceLibrary{
 
     public static void main(String[] args) {
         if (System.getSecurityManager() == null) {
@@ -19,9 +19,32 @@ public class ServiceLibrary {
         try {
             IRMIRegistry rmi = (IRMIRegistry) Naming.lookup("rmi://localhost:8080/MyRMI");
             Library service = new Library();
-            rmi.bind("Library", service);
+            rmi.rebind("Library", service);
+            System.out.println("IT's WORKING ! ");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
+
+/*
+try {
+        Hashtable properties = new Hashtable();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY,
+        "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        properties.put(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+        InitialContext context = new InitialContext(properties);
+        javax.jms.ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        javax.jms.Connection  connect = factory.createConnection();
+
+        javax.jms.Session sendSession = connect.createSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE);
+        javax.jms.Queue queue = (Queue) context.lookup("dynamicQueues/queueExo2");
+        javax.jms.MessageProducer sender = sendSession.createProducer(queue);
+
+        System.out.println("TEST_F");
+        }catch(Exception e){
+        e.printStackTrace();
+        }
+        */
