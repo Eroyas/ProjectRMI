b47f87f75f157063661ae03e996cb92827876423
Corentin <corentin.fouyaya@hotmail.fr>
2016-05-25 21:46:09 +0200

    partage de travail

--- Services/src/JMS/ServiceJMS.java
+++ Services/src/JMS/ServiceJMS.java
@@ -4,6 +4,7 @@ import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.io.Serializable;
 import java.util.Hashtable;
 
 /**
@@ -18,6 +19,54 @@ public class ServiceJMS {
 
     public ServiceJMS() throws JMSException {
         configurer();
+    }
+
+    public class OurConnect implements Serializable{
+
+        javax.jms.Connection connection;
+        public Session createSession(boolean b, int i) throws JMSException {
+            return null;
+        }
+
+        public String getClientID() throws JMSException {
+            return connection.getClientID();
+        }
+
+        public void setClientID(String s) throws JMSException {
+            connection.setClientID(s);
+        }
+
+        public ConnectionMetaData getMetaData() throws JMSException {
+            return connection.getMetaData();
+        }
+
+        public ExceptionListener getExceptionListener() throws JMSException {
+            return connection.getExceptionListener();
+        }
+
+        public void setExceptionListener(ExceptionListener exceptionListener) throws JMSException {
+            connection.setExceptionListener(exceptionListener);
+        }
+
+        public void start() throws JMSException {
+            connection.start();
+        }
+
+        public void stop() throws JMSException {
+            connection.stop();
+        }
+
+        public void close() throws JMSException {
+            connection.close();
+        }
+
+        public ConnectionConsumer createConnectionConsumer(Destination destination, String s, ServerSessionPool serverSessionPool, int i) throws JMSException {
+            return connection.createConnectionConsumer(destination, s, serverSessionPool, i);
+        }
+
+        public ConnectionConsumer createDurableConnectionConsumer(Topic topic, String s, String s1, ServerSessionPool serverSessionPool, int i) throws JMSException {
+            return connection.createDurableConnectionConsumer(topic, s, s1, serverSessionPool, i);
+        }
     }
 
     private void configurer() throws JMSException {
